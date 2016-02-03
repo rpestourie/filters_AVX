93ba955b89fc74692f0f98476f9d9ba0c601a485
tperol <tperol@seas.harvard.edu>
2016-02-03 17:36:28 -0500

    addding gaussian class into master repo

--- test.py
+++ test.py
@@ -1,20 +1,37 @@
+# ------------------------------------------------------------
+# import modules to compile cython and AVX
+# ------------------------------------------------------------
+
+import sys
+import os.path
+sys.path.append(os.path.join('.', 'util'))
+
+import set_compiler
+set_compiler.install()
+
+import pyximport
+pyximport.install()
+import pdb
+
+
 from scipy import misc
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 from scipy.ndimage.filters import gaussian_filter1d, gaussian_filter
-from Gaussianfilter2D import Gaussianfilter2D
-
+from Gaussianfilter2D_class import Gaussianfilter2D, filter_cython_threading
 
 # --------------------------------------------
 # initialization - define image
-# --------------------------------------------
+# --------------------------------------------*
 imagename = 'small.png'
 
 # load image (only one color now)
 f = misc.imread(imagename)
-f = f[:,:,0]
-
+if imagename == 'small.png':
+	f = f[:,:,0]
+else:
+	pass
 
 
 # --------------------------------------------
@@ -22,7 +39,7 @@ f = f[:,:,0]
 # --------------------------------------------
 
 # create a instance gaussian filter
-gb = Gaussianfilter2D(sigma = 4.0, truncate = 4.0, mode = 'reflect', cval = 0.0)
+gb = Gaussianfilter2D(sigma = 8.0, truncate = 8.0, mode = 'wrap', cval = 0.0, num_threads = 4)
 
 
 # --------------------------------------------
@@ -30,32 +47,34 @@ gb = Gaussianfilter2D(sigma = 4.0, truncate = 4.0, mode = 'reflect', cval = 0.0)
 # --------------------------------------------
 
 
-print 'lw', gb.lw 
-print 'pan', gb.pan
-print 'span', gb.span
-print 'f.shape', f.shape
+# print 'lw', gb.lw 
+# print 'pan', gb.pan
+# print 'span', gb.span
+# print 'f.shape', f.shape
 
-# gb.filter_python( f)
+gb.filter_python(f)
+print 'run time python' , gb.run_time_
 
-# print 'error compared to scipy', gb.error_
+gb.filter_cython(f)
+print 'run time cython' , gb.run_time_
 
-# print 'run time scipy' , gb.run_time_benchmark_
-# print 'run time python' , gb.run_time_
+gb.filter_AVX(f)
+print 'run time AVX' , gb.run_time_
 
-gb._other(f)
-print 'error other compared to scipy', gb.error_
+filter_cython_threading(gb,f)
+print 'run time cython with multithreading' , gb.run_time_	
 
 
 
-print 'kernel.shape', gb.kernel_.shape
-
-# --------------------------------------------
-# plot the images
-# --------------------------------------------
+# # --------------------------------------------
+# # plot the images
+# # --------------------------------------------
 
 fig, ax = plt.subplots(1,2)
 ax[0].imshow(gb.image_);
 ax[0].set_title('Python algorithm');
-ax[1].imshow(gb.image_benchmark_);
+ax[1].imshow(f);
 ax[1].set_title('Scipy library');
 plt.show()
+
+
